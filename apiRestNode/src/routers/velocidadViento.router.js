11b84800c9be4c9c1aca703b93a91b7d34ff1692
karensemanate <karen.semanate@soy.sena.edu.co>
2025-04-05 16:05:01 -0500

    borre las rutas de los controladores que no existen

--- apiRestNode/src/routers/velocidadViento.router.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createVelocidadViento, getAllVelocidadViento } from "../controllers/velocidadViento.controller.js";
-
-const velocidadViento = Router()
-velocidadViento.get("/velocidadViento",getAllVelocidadViento)
-velocidadViento.post("/velocidadViento",createVelocidadViento)
-export default velocidadViento;
