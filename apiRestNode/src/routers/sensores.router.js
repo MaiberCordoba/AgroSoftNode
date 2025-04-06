41c2c7a683e650db74f8ab8a19165e3b00a1c9e8
karensemanate <karen.semanate@soy.sena.edu.co>
2025-04-06 11:27:12 -0500

    verifique que todas las rutas encesitaran autenticacion para poder utiliarlas

--- apiRestNode/src/routers/sensores.router.js
+++ apiRestNode/src/routers/sensores.router.js
@@ -1,14 +1,14 @@
 import { Router } from "express"
 
 import { ListarSensores, RegistrarSensor, ActualizarSensor, EliminarSensor, ObtenerSensorPorId } from "../controllers/sensores.Controller.js";
-//import verifyJWT from '../middlewares/verifyJWT.middleware.js';
+import verifyJWT from '../middlewares/verifyJWT.middleware.js';
 
 const router = Router()
 
-router.get("/sensores", ListarSensores)
-router.get("/sensores/:id", ObtenerSensorPorId)
-router.post("/sensores", RegistrarSensor)
-router.put("/sensores/:id", ActualizarSensor)
-router.delete("/sensores/:id", EliminarSensor)
+router.get("/sensores",verifyJWT, ListarSensores)
+router.get("/sensores/:id",verifyJWT, ObtenerSensorPorId)
+router.post("/sensores",verifyJWT, RegistrarSensor)
+router.put("/sensores/:id",verifyJWT, ActualizarSensor)
+router.delete("/sensores/:id", verifyJWT, EliminarSensor)
 
 export default router
