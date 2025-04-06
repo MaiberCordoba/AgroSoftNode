0b852f2563cfc45ee87afeeb7c1d4450baff7362
Mamuel17 <rubyesperanza596@gmail.com>
2025-04-05 20:34:22 -0500

    se corrigio la ruta de la conxion del frotend con los datos del backend pata lotes y tipos especies

--- AgroSoft_f/src/modules/Trazabilidad/api/tiposEspecieApi.ts
+++ AgroSoft_f/src/modules/Trazabilidad/api/tiposEspecieApi.ts
@@ -2,22 +2,22 @@ import apiClient from "@/api/apiClient";
 import { TiposEspecie } from "../types";
 
 export const getTiposEspecie = async ():Promise<TiposEspecie[]> => {
-    const response = await apiClient.get("tiposespecie/");
+    const response = await apiClient.get("tiposEspecie/");
     return response.data
 };
 
 export const postTiposEspecie = async (data?:any):Promise<TiposEspecie> => {
-    const response = await apiClient.post<TiposEspecie>('tiposespecie/',data);
+    const response = await apiClient.post<TiposEspecie>('tiposEspecie/',data);
     return response.data
 }
 
 export const patchTiposEspecie = async ( id: number, data: Partial<TiposEspecie>): Promise<TiposEspecie> => {
-    const response = await apiClient.patch<TiposEspecie>(`tiposespecie/${id}/`, data);
+    const response = await apiClient.patch<TiposEspecie>(`tiposEspecie/${id}/`, data);
     return response.data;
   };
 
 
 export const deleteTiposEspecie = async (id: number): Promise<TiposEspecie> => {
-    const response = await apiClient.delete<TiposEspecie>(`tiposespecie/${id}/`);
+    const response = await apiClient.delete<TiposEspecie>(`tiposEspecie/${id}/`);
     return response.data
 }
