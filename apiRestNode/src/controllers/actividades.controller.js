0e402b801a027c9dbf36a42ee993c1604ae35834
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-06-15 17:07:35 -0500

    Cambio el formato de la fecha para que salga solo la fecha sin la hora

--- apiRestNode/src/controllers/actividades.controller.js
+++ apiRestNode/src/controllers/actividades.controller.js
@@ -1,60 +1,66 @@
 import pool from "../db.js";
 
-export const createActividad = async(req,res) => {
-    try{
-        const {fk_Cultivos,fk_Usuarios,titulo,descripcion,fecha,estado}=req.body
+export const createActividad = async (req, res) => {
+    try {
+        const { fk_Cultivos, fk_Usuarios, titulo, descripcion, fecha, estado } = req.body
         const sql = "INSERT INTO actividades (fk_Cultivos,fk_Usuarios,titulo,descripcion,fecha,estado) VALUES (?,?,?,?,?,?)"
-        const [rows] = await pool.query(sql,[fk_Cultivos,fk_Usuarios,titulo,descripcion,fecha,estado]) 
-        if (rows.affectedRows > 0){
-            return res.status(201).json({msg : "Actividad creada correctamente"})
+        const [rows] = await pool.query(sql, [fk_Cultivos, fk_Usuarios, titulo, descripcion, fecha, estado])
+        if (rows.affectedRows > 0) {
+            return res.status(201).json({ msg: "Actividad creada correctamente" })
         }
-        else{
-            return res.status(400).json({msg : "Error al crear la actividad"})
+        else {
+            return res.status(400).json({ msg: "Error al crear la actividad" })
         }
     }
-    catch(error){
+    catch (error) {
         console.error(error)
-        res.status(500).json({msg : "Internal server error"})
+        res.status(500).json({ msg: "Internal server error" })
     }
 }
 
-export const getAllActividad = async(req,res) => {
-    try{
-        const sql = "SELECT * FROM actividades"
-        const [rows] = await pool.query(sql)
-        if (rows.length > 0){
-            return res.status(200).json({rows})
+export const getAllActividad = async (req, res) => {
+    try {
+        const sql = "SELECT * FROM actividades";
+        const [rows] = await pool.query(sql);
+
+        if (rows.length > 0) {
+            // Formatear la fecha al formato YYYY-MM-DD
+            const actividadesFormateadas = rows.map((actividad) => ({
+                ...actividad,
+                fecha: actividad.fecha.toISOString().split("T")[0],
+            }));
+
+            return res.status(200).json({ rows: actividadesFormateadas });
+        } else {
+            return res.status(404).json({ msg: "No se encontraron actividades" });
         }
-        else{
-            return res.status(404).json({msg : "No se encontraron actividades"})
-        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Internal server error" });
     }
-    catch(error){
-        console.error(error)
-        res.status(500).json({msg : "Internal server error"})
-    }
-}
-export const updateActividad = async(req,res) => {
-    try{
+};
+
+export const updateActividad = async (req, res) => {
+    try {
         const id = req.params.id
-        const {fk_Cultivos,fk_Usuarios,titulo,descripcion,fecha,estado}=req.body
+        const { fk_Cultivos, fk_Usuarios, titulo, descripcion, fecha, estado } = req.body
         const sql = `UPDATE actividades SET fk_Cultivos=?,fk_Usuarios=?,titulo=?,descripcion=?,fecha=?,estado=? WHERE id = ${id}`
-        const [rows] = await pool.query(sql,[fk_Cultivos,fk_Usuarios,titulo,descripcion,fecha,estado]) 
-        if (rows.affectedRows > 0){
-            return res.status(201).json({msg : "Actividad actualizada correctamente"})
+        const [rows] = await pool.query(sql, [fk_Cultivos, fk_Usuarios, titulo, descripcion, fecha, estado])
+        if (rows.affectedRows > 0) {
+            return res.status(201).json({ msg: "Actividad actualizada correctamente" })
         }
-        else{
-            return res.status(400).json({msg : "Error al actualizar la actividad"})
+        else {
+            return res.status(400).json({ msg: "Error al actualizar la actividad" })
         }
     }
-    catch(error){
+    catch (error) {
         console.error(error)
-        res.status(500).json({msg : "Internal server error"})
+        res.status(500).json({ msg: "Internal server error" })
     }
 }
 
-export const reporteRentabilidad = async(req,res) => {
-    try{
+export const reporteRentabilidad = async (req, res) => {
+    try {
         const sql = `SELECT 
                     cu.nombre AS Cultivo,
                     (co.unidades * ve.precioUnitario) AS Ingresos,
@@ -70,22 +76,22 @@ export const reporteRentabilidad = async(req,res) => {
                     JOIN horasmensuales hm ON hm.fk_Pasantes = pa.id
                     JOIN usuarios us ON pa.fk_Usuarios = us.identificacion
                     `
-        const [rows] = await pool.query(sql) 
-        if (rows.length > 0){
+        const [rows] = await pool.query(sql)
+        if (rows.length > 0) {
             return res.status(200).json(rows)
         }
-        else{
-            return res.status(404).json({msg : "No se encontraron reportes de Rentantabilidad"})
+        else {
+            return res.status(404).json({ msg: "No se encontraron reportes de Rentantabilidad" })
         }
     }
-    catch(error){
+    catch (error) {
         console.error(error)
-        res.status(500).json({msg : "Internal server error"})
+        res.status(500).json({ msg: "Internal server error" })
     }
 }
 
-export const graficoActividades = async (req,res) => {
-    try{
+export const graficoActividades = async (req, res) => {
+    try {
         const sql = `SELECT 
                     ac.titulo AS Actividad, ac.fecha AS Fecha,hm.minutos AS Tiempo
                     FROM actividades ac
@@ -93,22 +99,22 @@ export const graficoActividades = async (req,res) => {
                     JOIN horasmensuales hm ON hm.fk_Pasantes = pa.id
                     JOIN usuarios us ON pa.fk_Usuarios = us.identificacion
                     `
-        const [rows] = await pool.query(sql) 
-        if (rows.length > 0){
+        const [rows] = await pool.query(sql)
+        if (rows.length > 0) {
             return res.status(200).json(rows)
         }
-        else{
-            return res.status(404).json({msg : "No se encontraron reportes"})
+        else {
+            return res.status(404).json({ msg: "No se encontraron reportes" })
         }
     }
-    catch(error){
+    catch (error) {
         console.error(error)
-        res.status(500).json({msg : "Internal server error"})
+        res.status(500).json({ msg: "Internal server error" })
     }
 }
 
-export const reporteCostoActividad = async (req,res) => {
-    try{
+export const reporteCostoActividad = async (req, res) => {
+    try {
         const sql = `SELECT 
                         ins.nombre AS Insumo,
                         ins.precio AS PrecioInsumo,
@@ -124,16 +130,16 @@ export const reporteCostoActividad = async (req,res) => {
                         JOIN usosProductos up ON up.fk_Actividades = ac.id
                         JOIN insumos ins ON up.fk_Insumos = ins.id;
                     `
-        const [rows] = await pool.query(sql) 
-        if (rows.length > 0){
+        const [rows] = await pool.query(sql)
+        if (rows.length > 0) {
             return res.status(200).json(rows)
         }
-        else{
-            return res.status(404).json({msg : "No se encontraron reportes"})
+        else {
+            return res.status(404).json({ msg: "No se encontraron reportes" })
         }
     }
-    catch(error){
+    catch (error) {
         console.error(error)
-        res.status(500).json({msg : "Internal server error"})
+        res.status(500).json({ msg: "Internal server error" })
     }
 }
