96376f34136b821d182224a05d00dd1c9c57bc6e
maiberCordoba <cordobamaiber@gmail.com>
2025-06-16 15:38:52 -0500

    modelos bd y controlador de usuarios con prisma

--- apiRestNode/src/db.js
+++ apiRestNode/src/db.js
@@ -1,13 +1,4 @@
-import { createPool } from "mysql2/promise";
-import dotenv from 'dotenv';
-dotenv.config();
-
-const pool = createPool({
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT,
-    database : process.env.DB_NAME,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASS,
-});
+import { PrismaClient } from "@prisma/client";
+const pool = new PrismaClient();
 
 export default pool;
