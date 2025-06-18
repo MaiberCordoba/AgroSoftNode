d681a64f18a04c882076f4097ec0c31780bf7993
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-06-18 15:27:25 -0500

    Consumo el endpoint de actividades por id

--- AgroSoft_f/src/modules/Finanzas/api/actividadesApi.ts
+++ AgroSoft_f/src/modules/Finanzas/api/actividadesApi.ts
@@ -7,17 +7,22 @@ export const getActividades = async (): Promise<Actividades[]> => {
   return response.data ?? []
 };
 
+export const getActividadById = async (id: number): Promise<Actividades | null> => {
+  const response = await apiClient.get(`actividades/${id}`);
+  return response.data ?? null;
+};
+
 export const postActividad = async (actividadesData: Partial<Actividades>): Promise<Actividades> => {
   const response = await apiClient.post("actividades", actividadesData);
   return response.data;
 };
 
-export const patchActividades = async ( id: number, data: Partial<Actividades>): Promise<Actividades> => {
-    const response = await apiClient.patch<Actividades>(`actividades/${id}/`, data);
-    return response.data;
-  };
+export const patchActividades = async (id: number, data: Partial<Actividades>): Promise<Actividades> => {
+  const response = await apiClient.patch<Actividades>(`actividades/${id}/`, data);
+  return response.data;
+};
 
-  export const deleteActividades = async (id: number): Promise<Actividades> => {
-    const response = await apiClient.delete<Actividades>(`actividades/${id}/`);
-    return response.data
+export const deleteActividades = async (id: number): Promise<Actividades> => {
+  const response = await apiClient.delete<Actividades>(`actividades/${id}/`);
+  return response.data
 }
